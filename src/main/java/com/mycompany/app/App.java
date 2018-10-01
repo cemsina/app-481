a618d54a3da1168030e2301de0e4cb127ca9751c
Cemsina <cemsina@cemsina.com>
2018-10-01 16:35:00 +0300

    lottery

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -103,35 +103,15 @@ public class App {
 
 	public static void main(String[] args) {
 		port(getHerokuAssignedPort());
-
 		get("/", (req, res) -> "Hello, World");
-
 		post("/compute", (req, res) -> {
-			// System.out.println(req.queryParams("input1"));
-			// System.out.println(req.queryParams("input2"));
-			String input1 = req.queryParams("input1");
-			java.util.Scanner sc1 = new java.util.Scanner(input1);
-			sc1.useDelimiter("[;\r\n]+");
-			java.util.ArrayList<Integer> inputList = new java.util.ArrayList<Integer>();
-			while (sc1.hasNext()) {
-				int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
-				inputList.add(value);
-			}
-			System.out.println(inputList);
-			String input2 = req.queryParams("input2").replaceAll("\\s", "");
-			int input2AsInt = Integer.parseInt(input2);
-			String result = App.search(inputList, input2AsInt) + "";
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("result", result);
-			return new ModelAndView(map, "compute.mustache");
+			return new ModelAndView(App.lotteryRequestParser(req), "compute.mustache");
 		}, new MustacheTemplateEngine());
-
 		get("/compute", (rq, rs) -> {
 			Map map = new HashMap();
 			map.put("result", "not computed yet!");
 			return new ModelAndView(map, "compute.mustache");
 		}, new MustacheTemplateEngine());
-
 	}
 
 	static int getHerokuAssignedPort() {
